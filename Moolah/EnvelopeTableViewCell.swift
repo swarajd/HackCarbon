17bef52f192d4d95ebfa356594a17595cb0cc01b
jsun27 <jdsun@vt.edu>
2017-06-07 19:13:48 -0400

    set up for Summary in Storyboard and swift classes. Also added the Add pages

---
+++ Moolah/EnvelopeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EnvelopeTableViewCell.swift
+//  Moolah
+//
+//  Created by Sun, Jessica on 6/7/17.
+//  Copyright © 2017 Sun, Jessica. All rights reserved.
+//
+
+import UIKit
+
+class EnvelopeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
