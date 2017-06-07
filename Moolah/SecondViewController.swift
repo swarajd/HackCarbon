019725d6d0434b377e5f1a9a8e5ed08528f7c11d
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2017-06-07 09:57:34 -0400

    basic xcode setup

---
+++ Moolah/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Moolah
+//
+//  Created by Dhumne, Swaraj on 6/7/17.
+//  Copyright © 2017 Sun, Jessica. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
