cb980cc2251a847806bd4f5a51be718d57824f91
askrish2 <askrish2@illinois.edu>
2017-06-07 12:51:14 -0400

    m

---
+++ Moolah/Home.swift
@@ -0,0 +1,13 @@
+//
+//  Home.swift
+//  Moolah
+//
+//  Created by Krishnan, Ashwini on 6/7/17.
+//  Copyright © 2017 Sun, Jessica. All rights reserved.
+//
+
+import UIKit
+
+class Home: NSObject {
+
+}
---
+++ Moolah/TipsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TipsViewController.swift
+//  Moolah
+//
+//  Created by Krishnan, Ashwini on 6/7/17.
+//  Copyright © 2017 Sun, Jessica. All rights reserved.
+//
+
+import UIKit
+
+class TipsViewController: UIViewController {
+
+    @IBOutlet var shopButton: UIButton!
+    @IBOutlet var couponButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func clickedShopButton(_ sender: UIButton) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
