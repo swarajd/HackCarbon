96b2c8a0131709b09278059738d9dee47868e534
vvpitta <vvpitta@gmail.com>
2017-06-08 09:16:59 -0400

    force push

---
+++ Moolah/CouponViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CouponViewController.swift
+//  Moolah
+//
+//  Created by Varun Pitta on 6/8/17.
+//  Copyright © 2017 Sun, Jessica. All rights reserved.
+//
+
+import UIKit
+
+class CouponViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
